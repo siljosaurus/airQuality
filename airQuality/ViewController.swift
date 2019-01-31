dbd76e7075c73599d1e0f4d642315a8715ca8e16
Silje Marie Flaaten <silje@Siljes-MacBook-Pro.local>
2019-01-31 21:52:11 +0100

    First Commit

---
+++ airQuality/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  airQuality
+//
+//  Created by Silje Marie Flaaten on 31/01/2019.
+//  Copyright © 2019 Silje Marie Flaaten. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
